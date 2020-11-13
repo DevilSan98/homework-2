c08050a3460d255bc736bc25ff66610ce62a347e
Todou <bodya.shebet@gmail.com>
2020-11-13 14:56:48 +0200

    FZ4NZ8ZYY7

---
+++ main.js
@@ -0,0 +1,89 @@
+var a = 5;  
+var b, c;
+
+b = (a * 5);  
+b = (c = b/2); 
+
+
+var age;
+var data = 2020;
+var DB
+    
+    var age  = prompt("сколько вам лет.",""); 
+	  DB  = data - age;                
+    alert ("Дата рождения" + DB);   
+    
+var tempera;
+var fareng;
+    
+    var fareng = prompt("Сколько градусов.","")
+    fareng = (fareng * 0.556)
+    alert ("F" + fareng)
+
+
+var numb1;
+var numb2;
+var numb1 = prompt("Какое первое число.","")
+var numb1 = prompt("Какое второе число.","")
+    
+        alert(Math.floor( numb1 * numb2))
+
+
+var number1 = prompt("Введите число.","")
+
+function isEven(number1) {
+	if (number1%2 == 0)
+		return true;
+	else
+        return false;
+}
+
+var name = prompt("Как тебя зовут.","")
+
+        alert("Приветствуем"+ name)
+
+
+        var word;
+        var word = confirm("20 = 20.","")
+                if(confirm == true)        
+                alert("Hellow")
+                else(alert("You cansle this"))
+        
+var Sex1;
+
+var Sex1 = confirm("Are u a guy?")
+            if(confirm == true)
+            alert("Helllow brother")
+            else(confirm("Are u a girl?",""))
+            if(confirm == true)
+            alert("Hello sister")
+            else(alert(" Get out here omuncul!!"))
+        
+             var person = {
+                name: "Ivan",
+                surname: "Ivanov",
+                age: 25
+            }
+            
+            person.name = 'vova'
+            alert(person.name);
+
+var age = +prompt("Сколько вам лет?","");
+
+if (age < 18){
+    alert("школьник, малышь");
+}
+else 
+    if (age < 45){
+    alert("adult");
+}
+else 
+    if (age < 80){
+    alert("закат,пенсия");
+}
+else {
+    alert("то ли киборг, то ли ошибка"); 
+}
+                
+
+
